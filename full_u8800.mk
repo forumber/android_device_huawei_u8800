e8c326cb48f62ca8810f250cdc5ed3e0abf1062a
forumber <kidobeko@gmail.com>
2013-02-04 17:21:04 +0200

    u8800: fix dexopt

--- full_u8800.mk
+++ full_u8800.mk
@@ -31,7 +31,8 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_PROPERTY_OVERRIDES += \
         dalvik.vm.heapstartsize=5m \
         dalvik.vm.heapgrowthlimit=48m \
-        dalvik.vm.heapsize=128m
+        dalvik.vm.heapsize=128m \
+        dalvik.vm.dexopt-data-only=1
 
 # USB mass storage
 ADDITIONAL_DEFAULT_PROPERTIES += \
