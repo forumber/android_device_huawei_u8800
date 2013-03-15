7e1a7a9a4770a9048684f7d40dfb618a0eae66a7
forumber <kidobeko@gmail.com>
2013-03-15 21:43:32 +0200

    u8800: add audio drivers (Thanks dzo)

--- full_u8800.mk
+++ full_u8800.mk
@@ -49,8 +49,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
 	audio.a2dp.default \
-	audio_policy.msm7x30 \
-	audio.primary.msm7x30 \
+	audio_policy.u8800 \
+	audio.primary.u8800 \
 	libaudioutils
 
 # Lights
